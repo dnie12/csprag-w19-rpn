c4afd8aa3ac4a1779f936e15963be69ca9ad3262
dnie <dnie@umich.edu>
2019-03-06 16:21:27 -0500

    Failing for keys

--- test_rpn.py
+++ test_rpn.py
@@ -18,3 +18,21 @@ class TestBasics(unittest.TestCase):
     def test_exponent(self):
         result = rpn.calculate("2 3 ^")
         self.assertEqual(8, result)
+    def test_factorial(self):
+        result = rpn.calculate("4 !")
+        self.assertEqual(24, result)
+    def test_integer_division(self):
+        result = rpn.calculate("3 2 //")
+        self.assertEqual(1, result)
+    def test_percentage(self):
+        result = rpn.calculate("72 + 5%")
+        self.assertEqual(75.6, result)
+    def test_bitwise_and(self):
+        result = rpn.calculate("5 & 7")
+        self.assertEqual(5, result)
+    def test_bitwise_or(self):
+        result = rpn.calculate("5 | 7")
+        self.assertEqual(7, result)
+    def test_bitwise_not(self):
+        result = rpn.calculate("~ 5")
+        self.assertEqual(-6, result)
